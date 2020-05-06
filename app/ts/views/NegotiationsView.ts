48bc413335eece253a024a11fbabc12b90af150b
Leonardo Freitas <leonardo.cadu2@hotmail.com>
2020-05-06 19:36:57 -0300

    add namespace

--- app/ts/views/NegotiationsView.ts
+++ app/ts/views/NegotiationsView.ts
@@ -1,38 +1,40 @@
-class NegotiationsView extends View<Negotiations> {
-  template(model: Negotiations): string {
-    return `
-    <table class="table table-hover table-bordered">
-      <thead>
-          <tr>
-              <th>DATA</th>
-              <th>QUANTIDADE</th>
-              <th>VALOR</th>
-              <th>VOLUME</th>
-          </tr>
-      </thead>
-
-      <tbody>
-        ${model
-          .toArray()
-          .map((negotiation) => {
-            return `
-          <tr>
-            <td>${negotiation.data.getDate()}/${
-              negotiation.data.getMonth() + 1
-            }/${negotiation.data.getFullYear()}
-            </td>
-            <td>${negotiation.quantity}</td>
-            <td>${negotiation.value}</td>
-            <td>${negotiation.volume}</td>
-          </tr>
-        `;
-          })
-          .join("")}
-      </tbody>
-
-      <tfoot>
-      </tfoot>
-    </table>
-    `;
+namespace Views {
+  export class NegotiationsView extends View<Negotiations> {
+    template(model: Negotiations): string {
+      return `
+      <table class="table table-hover table-bordered">
+        <thead>
+            <tr>
+                <th>DATA</th>
+                <th>QUANTIDADE</th>
+                <th>VALOR</th>
+                <th>VOLUME</th>
+            </tr>
+        </thead>
+  
+        <tbody>
+          ${model
+            .toArray()
+            .map((negotiation) => {
+              return `
+            <tr>
+              <td>${negotiation.data.getDate()}/${
+                negotiation.data.getMonth() + 1
+              }/${negotiation.data.getFullYear()}
+              </td>
+              <td>${negotiation.quantity}</td>
+              <td>${negotiation.value}</td>
+              <td>${negotiation.volume}</td>
+            </tr>
+          `;
+            })
+            .join("")}
+        </tbody>
+  
+        <tfoot>
+        </tfoot>
+      </table>
+      `;
+    }
   }
 }
