142e106341d4b68a5e7243b472fc134b8b50ebfe
Leonardo Freitas <leonardo.cadu2@hotmail.com>
2020-05-06 19:29:58 -0300

    add jquery

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,5 +1,3 @@
 const controller = new NegotiationController();
 
-document
-  .querySelector(".form")
-  .addEventListener("submit", controller.adiciona.bind(controller));
+$(".form").submit(controller.adiciona.bind(controller));
--- app/ts/controllers/NegotiationController.ts
+++ app/ts/controllers/NegotiationController.ts
@@ -1,15 +1,15 @@
 class NegotiationController {
-  private _inputData: HTMLInputElement;
-  private _inputQuantity: HTMLInputElement;
-  private _inputValue: HTMLInputElement;
+  private _inputData: JQuery;
+  private _inputQuantity: JQuery;
+  private _inputValue: JQuery;
   private _negotiations = new Negotiations();
   private _negotiationsView = new NegotiationsView("#negotiationsView");
   private _messageView = new MessageView("#messageView");
 
   constructor() {
-    this._inputData = <HTMLInputElement>document.querySelector("#data");
-    this._inputQuantity = <HTMLInputElement>document.querySelector("#quantity");
-    this._inputValue = <HTMLInputElement>document.querySelector("#value");
+    this._inputData = $("#data");
+    this._inputQuantity = $("#quantity");
+    this._inputValue = $("#value");
 
     // renderiza a tabela assim que carregar
     this._negotiationsView.update(this._negotiations);
@@ -19,9 +19,9 @@ class NegotiationController {
     event.preventDefault();
 
     const negotiation = new Negotiation(
-      new Date(this._inputData.value.replace(/-/g, ",")),
-      parseInt(this._inputQuantity.value),
-      parseFloat(this._inputValue.value)
+      new Date(this._inputData.val().toString().replace(/-/g, ",")),
+      parseInt(this._inputQuantity.val().toString()),
+      parseFloat(this._inputValue.val().toString())
     );
 
     this._negotiations.adiciona(negotiation);
